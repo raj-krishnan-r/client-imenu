ea1a889b0557fb72931ed0e139c9d4f217f278eb
Your Name <you@example.com>
2017-08-17 12:25:40 +0530

    Wifi connection methods updated

--- app/src/main/java/com/example/raj/qrtowifi/wifi2Ip.java
+++ app/src/main/java/com/example/raj/qrtowifi/wifi2Ip.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import static android.content.Context.WIFI_SERVICE;
 
@@ -18,12 +19,17 @@ import static android.content.Context.WIFI_SERVICE;
 public class wifi2Ip extends AsyncTask<String,String,String> {
 Context cxt;
     int tid;
-public wifi2Ip(Context context,int tableid)
+    WifiManager wifiManager;
+
+    public wifi2Ip(Context context,int tableid)
 {
     cxt=context;
     tid = tableid;
+    wifiManager =  (WifiManager) cxt.getSystemService(Context.WIFI_SERVICE);
 }
     protected String doInBackground(String... args) {
+
+        /*
         WifiConfiguration wifiConfig = new WifiConfiguration();
         wifiConfig.SSID = String.format("\"%s\"", args[0]);
         wifiConfig.preSharedKey = String.format("\"%s\"",args[1]);
@@ -35,6 +41,26 @@ public wifi2Ip(Context context,int tableid)
         wifiManager.disconnect();
         wifiManager.enableNetwork(netId, true);
         wifiManager.reconnect();
+        */
+        String networkSSID = args[0];
+        String networkPass = args[1];
+
+        // setup a wifi configuration
+        WifiConfiguration wc = new WifiConfiguration();
+        wc.SSID = "\""+networkSSID+"\"";
+        wc.preSharedKey = "\""+networkPass+"\"";
+        wc.status = WifiConfiguration.Status.ENABLED;
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        // connect to and enable the connection
+        int netId = wifiManager.addNetwork(wc);
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.setWifiEnabled(true);
+        wifiManager.reconnect();
         return args[2];
 
     }
@@ -43,6 +69,7 @@ public wifi2Ip(Context context,int tableid)
         URL url;
         try {
             url = new URL("http://" + res + ":3000/listings");
+            wifiManager.reconnect();
             new getData(cxt,tid).execute(url);
 
         } catch (MalformedURLException e) {
